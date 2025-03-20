d89e904f70c08a37303479787a1d6fe904d889c4
celineblle <celineblle@outlook.fr>
2025-03-20 16:53:47 +0100

    feat(grill): creation des timers du grill

--- src/components/cuisine/Grill.tsx
+++ src/components/cuisine/Grill.tsx
@@ -1,18 +1,120 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./Grill.css";
-import { viande } from "../../elements/stocks";
+//import { taille } from "../../elements/stocks";
 
 function Grill() {
+
+  const viande: string[] = [
+    "oeuf",
+    "jambon",
+    "steak",
+    "mayo",
+    "ketchup",
+    "moutarde",
+    "pain",
+    "salade",
+    "bougie",
+  ];
+  const limitSizeGrill: number = 8;
+  let standByTimeOut: number = 0;
   const [toggleTabGrill, setToggleTabGrill] = useState<string>("cuisson");
-  const [plaqueDeCuisson, setPlaqueDeCuisson] = useState<string[]>([]);
 
   function handleClickTabButtonGrill(element: string): void {
     setToggleTabGrill(element);
   }
 
-  function handleClickFrigoToGrill(element: string): void {
-    setPlaqueDeCuisson([...plaqueDeCuisson, element]);
+  const [plaqueDeCuisson, setPlaqueDeCuisson] = useState<string[]>([]);
+  const [plaqueDeCuissonPret, setPlaqueDeCuissonPret] = useState<string[]>([]);
+  const [plaqueDeCuissonGrille, setPlaqueDeCuissonGrille] = useState<string[]>(
+    []
+  );
+  const [placeVideGrill, setPlaceVideGrill] = useState<string[]>([]);
+  const [timeOutPretId, setTimeOutPretId] = useState<number[]>([])
+
+  const tabCuisson = useRef<string[]>([]);
+  const tabPret = useRef<string[]>([]);
+  const tabGrille = useRef<string[]>([]);
+  const timeOutPretRef = useRef<number[]>([])
+
+  useEffect(() => {
+    tabCuisson.current = plaqueDeCuisson;
+  }, [plaqueDeCuisson]);
+
+  useEffect(() => {
+    tabPret.current = plaqueDeCuissonPret;
+  }, [plaqueDeCuissonPret]);
+
+  useEffect(() => {
+    tabGrille.current = plaqueDeCuissonGrille;
+  }, [plaqueDeCuissonGrille]);
+
+  useEffect(() => {
+    timeOutPretRef.current = timeOutPretId;
+  }, [timeOutPretId])
+
+
+  function steakCuitStandBy(element: string): void {
+    standByTimeOut = setTimeout(() => {
+        setPlaqueDeCuissonGrille([...tabGrille.current, element]);
+        const oldestSteak: number = tabPret.current.indexOf(element);
+        const tabPretCopie: string[] = tabPret.current.slice();
+        tabPretCopie.splice(oldestSteak, 1);
+        setPlaqueDeCuissonPret(tabPretCopie);
+      }, 10000);
+      setTimeOutPretId([...timeOutPretRef.current, standByTimeOut]);
   }
+
+  function handleClickFrigoToGrill(element: string): void {
+    const actualSizeGrill: number =
+      tabCuisson.current.length +
+      tabPret.current.length +
+      tabGrille.current.length;
+
+    if (actualSizeGrill < limitSizeGrill) {
+      setPlaqueDeCuisson([...plaqueDeCuisson, element]);
+      setTimeout(() => {
+        setPlaqueDeCuissonPret([...tabPret.current, element]);
+        const oldestSteak: number = tabCuisson.current.indexOf(element);
+        const tabCuissonCopie: string[] = tabCuisson.current.slice();
+        tabCuissonCopie.splice(oldestSteak, 1);
+        setPlaqueDeCuisson(tabCuissonCopie);
+        steakCuitStandBy(element);
+      }, 2000);
+    }
+  }
+
+  useEffect(() => {
+    const actualSizeGrill: number =
+      tabCuisson.current.length +
+      tabPret.current.length +
+      tabGrille.current.length;
+    const placeVide: string[] = []
+    if (actualSizeGrill < limitSizeGrill) {
+      for (let i = actualSizeGrill; i < limitSizeGrill; i++) {
+
+        placeVide.push("Vide");
+      }
+    }
+    setPlaceVideGrill(placeVide)
+  }, [plaqueDeCuisson, plaqueDeCuissonPret, plaqueDeCuissonGrille]);
+
+  function handleClickAvailabilitySteak(element: string): void {
+    const getReadySteak: number = tabPret.current.indexOf(element);
+    const timeOutIdCopie: number[] = timeOutPretRef.current.slice();
+    const steakTimeOutId = timeOutIdCopie[getReadySteak]
+    clearTimeout(steakTimeOutId)
+    const tabCuissonCopie: string[] = tabPret.current.slice();
+    tabCuissonCopie.splice(getReadySteak, 1)
+    setPlaqueDeCuissonPret(tabCuissonCopie)
+  }
+  const styleTest = {
+    color: "blue"
+  }
+
+  console.log("plaque", plaqueDeCuisson);
+  console.log("pret", plaqueDeCuissonPret);
+  console.log("grille", plaqueDeCuissonGrille)
+
 
   return (
     <div id="grillComponent">
@@ -40,27 +142,38 @@ function Grill() {
           }
           id="CuissonGrill"
         >
-          <p>plaque de cuisson</p>
+          {plaqueDeCuisson.map((emplacement: string, index: number) => (
+            <button disabled={true} key={index}>
+              {emplacement}
+            </button>
+          ))}
+          {plaqueDeCuissonPret.map((emplacement: string, index: number) => (
+            <button key={index} onClick={() => handleClickAvailabilitySteak(emplacement)}>{emplacement}</button>
+          ))}
+          {plaqueDeCuissonGrille.map((emplacement: string, index: number) => (
+            <button style={styleTest} key={index}>{emplacement}</button>
+          ))}
+            {placeVideGrill.map((emplacement: string, index: number) => (
+            <button disabled={true} key={index}>{emplacement}</button>
+          ))}
+
         </div>
         <div
-         
           className={
             toggleTabGrill === "frigo"
               ? "tabGrillContent"
               : "tabGrillContentHidden"
           }
-           id="frigoGrill"
+          id="frigoGrill"
         >
-          
-            {viande.map((element) => (
-              <div  key={element}>
-                <button onClick={() => handleClickFrigoToGrill(element)}>
-                  {element}
-                </button>
-                <p>quantité : x</p>
-              </div>
-            ))}
-          
+          {viande.map((element) => (
+            <div key={element}>
+              <button onClick={() => handleClickFrigoToGrill(element)}>
+                {element}
+              </button>
+              <p>quantité : x</p>
+            </div>
+          ))}
         </div>
       </div>
       <div>
