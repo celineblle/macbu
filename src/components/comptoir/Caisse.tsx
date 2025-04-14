5af87c3030dbb245a017d8104245e2fc97ed3dcc
celineblle <celineblle@outlook.fr>
2025-04-14 12:46:13 +0200

    refactor(caisse): css page caisse

--- src/components/comptoir/Caisse.tsx
+++ src/components/comptoir/Caisse.tsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Caisse.css";
+import close from "../../assets/close.svg";
 
-function Caisse () {
+function Caisse() {
+  const [buttonActionModalCaisse, setButtonActionModalCaisse] =
+    useState<boolean>(false);
 
-    return ( <div id="caisseComponent" className="component">
-        <button className="buttonModal">caisse</button>
-        <p>caisse</p> 
-        </div>)
+  function handleClickActionModal(): void {
+    setButtonActionModalCaisse(!buttonActionModalCaisse);
+  }
+
+  return (
+    <div id="caisseComponent" className="component">
+      <button className="buttonModal" onClick={handleClickActionModal}>
+        Caisse
+      </button>
+      <ul id="listeCommandePage">
+        <li className="commandeUniquePage">Commande x : xx€</li>
+        <li className="commandeUniquePage">Commande x : xx€</li>
+        <li className="commandeUniquePage">Commande x : xx€</li>
+      </ul>
+
+      <div className={buttonActionModalCaisse ? "modalOpen" : "modalClose"}>
+        <div className="modalContent">
+          <div id="headerModal">
+            <h2>Caisse</h2>
+            <button
+              onClick={handleClickActionModal}
+              className="closeModalButton"
+            >
+              {" "}
+              <img alt="fermer" title="fermer" src={close}></img>
+            </button>
+          </div>
+          <p>caisse</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Caisse;
