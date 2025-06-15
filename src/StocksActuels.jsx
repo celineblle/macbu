1d13075a3ca9ca7f45d2a1d7670d187e16f750e5
celineblle <celineblle@outlook.fr>
2025-06-15 11:56:33 +0200

    creation des stocks en state

---
+++ src/StocksActuels.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect, useRef } from "react";
+
+function StocksActuels() {
+
+const [stocksCuisine, setStocksCuisine] = useState(
+    [
+
+    ]
+);
+
+const [stocksComptoir, setStocksComptoir] = useState()
+
+
+
+
+}
+
+export default StocksActuels
